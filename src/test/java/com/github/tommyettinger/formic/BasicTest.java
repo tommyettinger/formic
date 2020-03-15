78985b6ffbed89ed2580afa112a78fa3b55c36f9
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-03-14 20:43:22 -0700

    Added some simple tests, already found stuff to fix

---
+++ src/test/java/com/github/tommyettinger/formic/BasicTest.java
@@ -0,0 +1,19 @@
+package com.github.tommyettinger.formic;
+
+import org.junit.Test;
+
+public class BasicTest {
+	@Test
+	public void testString(){
+		FormatString format = FormatString.compile("%10s");
+		System.out.println(format.format("foo"));
+	}
+	@Test
+	public void testInt() {
+		FormatString format = FormatString.compile("%+012d");
+		System.out.println(format.format(-0x80000000) + " should be " + String.format("%+012d", -0x80000000));
+		System.out.println(format.format(-0x80000000L) + " should be " + String.format("%+012d", -0x80000000L));
+		System.out.println(format.format(0x7FFFFFFF) + " should be " + String.format("%+012d", 0x7FFFFFFF));
+		System.out.println(format.format(0x7FFFFFFFL) + " should be " + String.format("%+012d", 0x7FFFFFFFL));
+	}
+}
